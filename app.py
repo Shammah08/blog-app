ec66338763a242a07ccabd0cc4c4e81bf27c4b10
Shammah08 <shammahodhiambo@gmail.com>
2020-12-22 00:13:19 +0300

    Login and Signup functionality

--- app.py
+++ app.py
@@ -1,49 +1,54 @@
-from flask import Flask, render_template, request, redirect, escape, abort,url_for
+from flask import Flask, render_template, request, redirect, escape, abort,url_for,session
 from  methods import *
+import hashlib
 
 app = Flask(__name__)
-"""
-Check request method and not GET page when not signed in
-"""
-count = len(get_post())
+#FOR SESSIONS
+app.secret_key = 'MyVerySecretKey'
+
 #TEMPLATE ROUTES
-@app.route('/')
-def root():
-    blog = get_post()
-    count= len(blog)
-    recent = []
-    for k,v  in enumerate(blog):
-        if int(k) <5:
-            recent.append(v)
-    return render_template('home.html', recent = recent, count=count)
 #LOGIN PAGE
-@app.route('/login-page')
-def login():
-    return render_template('login.html')
-#SIGN UP PAGE
-@app.route('/signup-page')
-def sign():
-    return render_template('signup.html')
+@app.route('/')
+@app.route('/login', methods = ['POST','GET'])
+def log():
+    count = len(get_post())
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        username = request.form['username']
+        password = request.form['password']
+        response = log_in(username, password)
+        print('This is the response: ', response)
+        session['username'] = username
+        print('Session ni ya uyu mguys' , session['username'])
+        if response == 'Wrong password':
+            return  render_template('login.html',response = response)
+        elif response == 'Username not found':
+            return render_template('login.html', response= response)
+        else:
+            recent = response
+            #print('response[0]:',recent)
+            return render_template('home.html', username = username, recent= recent, count = count)
+        
+
 #ADMIN PAGE
 @app.route('/admin')
 def admin():
     return render_template('admin.html')
-    
-@app.route('/login', methods = ['POST','GET'])
-def log():
-    username = request.form['username']
-    password = request.form['password']
-    return log_in(username, password)
 
-@app.route('/signup', methods=['POST'])
+@app.route('/signup', methods=['POST','GET'])
 def signup():
-    fname  = request.form['fname']
-    lname = request.form['lname']
-    username = fname +  lname
-    email = request.form['email']
-    password = request.form['password']
-    sign_up(fname,lname,email,password,time_stamp)
-    return render_template('home.html', username = (username))
+    if request.method == 'GET':
+        return render_template('signup.html')   
+    else:
+        fname  = request.form['fname']
+        lname = request.form['lname']
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        code = hashlib.sha256(f'{str(password)}'.encode()).hexdigest()
+        sign_up(fname,lname,email,username,code)
+        return redirect(url_for('blog'))
 #VIEW BLOG
 @app.route('/blog')
 def blog():
@@ -51,9 +56,17 @@ def blog():
     count = len(post)
     return render_template('blog.html', post = post, count= count)
 #CREATE NEW POST
-@app.route('/blog/create')
+@app.route('/blog/create',methods= ['GET','POST'])
 def create():
-    return render_template('newposts.html')
+    if request.method == 'GET':
+        return render_template('newposts.html')
+    else:
+        author = request.form['author']
+        title = request.form['title']
+        content = request.form['content']
+        create_post(author,title,content)
+        return redirect(url_for('blog'))
+
 
 @app.route('/clear',methods = ['GET'])
 def clear():
@@ -75,6 +88,7 @@ def search(keyword):
 #GET POST
 @app.route('/blog/<int:id>')
 def post(id):
+    count= len(get_post())
     with DbManager(**DBCONGIF) as cursor:
         SQL = '''SELECT * FROM post WHERE post_id = %s'''
         cursor.execute(SQL,(id,))
@@ -86,12 +100,13 @@ def post(id):
     #EDIT POST
 @app.route('/blog/edit/<int:id>', methods  = ['GET', 'POST'])
 def edit(id):
+    count = len(get_post())
     with DbManager(**DBCONGIF) as cursor:
         if request.method == 'POST':       
             author = request.form['author']
             title = request.form['title']
             content = request.form['content']
-            SQL = '''UPDATE  post set author = %s, title = %s, content = %s where post_id = %s'''
+            SQL = '''UPDATE  post SET author = %s, title = %s, content = %s where post_id = %s'''
             cursor.execute(SQL,(author,title,content,id,)) 
             SQL_2 = '''SELECT * FROM post WHERE post_id = %s '''
             cursor.execute(SQL_2,(id,))
@@ -114,16 +129,8 @@ def delete(id):
 def view_data():
     username = request.form['username']
     password = request.form['password']
-    data = view_log(username,password)
-    return render_template('admin.html', data = data) 
-
-@app.route('/post', methods=['POST','GET'])
-def new_post():
-    author = request.form['author']
-    title = request.form['title']
-    content = request.form['content']
-    save = create_post(author,title,content)
-    return redirect(url_for('blog'))
+    log_data = view_log(username,password)
+    return render_template('admin.html', log_data = log_data) 
 
 @app.route('/play/search', methods=['POST'])
 def search_letter():
@@ -149,10 +156,7 @@ def bmi():
     weight = int(request.form['weight'])
     height = float(request.form['height'])   
     bmi = bmi_calc(name,weight,height)
-    return render_template('home.html', bmi = bmi)
+    return render_template('play.html', bmi = bmi)
 
 if __name__ == '__main__':
     app.run(port = 3000,debug=True)
-
-
-root()
--- methods.py
+++ methods.py
@@ -25,61 +25,64 @@ class DbManager():
         self.conn.close()
 
 time_stamp = datetime.now().strftime('%c')
-users = {
-    'steve': '1234',
-    "shammah": '1234',
-    'admin' : 'admin',
-    'superuser': '0000'
-}
+
 #activity log func
-def log_details(username:str,action:str):
-    with open('../log.log','a') as log:
-        print(f'{username.upper()} accessed the log on {time_stamp}\nAction: {action.upper()}|', file=log, end='\n',sep = '|')
-        #print(username.form, username.remote_addr, username.user_agent, file=log, end='\n')
-    return 'success'
 #sign_up function to log details in txt file    
-def sign_up(fname:str,lname:str,email:str,password:str,time_stamp:str):
+def sign_up(fname:str,lname:str,email:str,username:str,password:str,time= str):
     with DbManager(**DBCONGIF) as cursor:
-        signup = '''INSERT INTO signup (first_name,last_name, email,password) 
-        VALUES
-        (%s,%s,%s,%s)'''
-        return cursor.execute(signup,(fname,lname,email,password))
+        LOG_SQL ='''INSERT INTO  log (Action_done,username) VALUES(%s,%s)'''
+        cursor.execute(LOG_SQL,('SIGN UP',(username)))
+        SQL = '''INSERT INTO users (first_name,last_name, email,username,password) 
+        VALUES (%s,%s,%s,%s,%s)'''
+        return cursor.execute(SQL,(fname,lname,email,username,password))
         
 #log in func takes in username and password
-def log_in(username,password):
-    for account in users:
-        #ADD ADMIN LOG IN 
-        if username in users:   #check username
-            if users[username] == password: #check password
-                log_details(username,'login')
-                with DbManager(**DBCONGIF) as cursor:
-                    log = '''INSERT INTO login (username, password)
-                    VALUES (%s,%s)'''
-                    cursor.execute(log,(username,password))
-                return render_template('home.html', username = username)
+def log_in(username:str,password:str):
+     with DbManager(**DBCONGIF) as cursor:
+        LOG_SQL ='''INSERT INTO  log (Action_done,username) VALUES(%s,%s)'''
+        cursor.execute(LOG_SQL,('LOG IN',username))
+        USER_SQL = '''SELECT username, password FROM users  '''
+        cursor.execute(USER_SQL)
+        users = cursor.fetchall()
+        for user in users:
+            if user[0] == username:
+                if  user[1] == hashlib.sha256(password.encode()).hexdigest():
+                    blog = get_post()
+                    count =  len(blog)
+                    recent = []
+                    for k,v  in enumerate(blog):
+                        if int(k) <5:
+                            recent.append(v)
+                    return recent
+                else:
+                    response = 'Wrong password'
+                    return response
+                    break
             else:
-                return'wrong password'
-        else:
-            return 'User not found'
-#func to view log
+                response = 'Username not found'
+                return response
+                break
+
+#UPDATE VIEW LOG FUNTION
 def view_log(username,password):
-    log_details(username, 'view log')
-    if username in users:
-        if password == users[username]:
-            with open('log.log') as vlog:
-                content = vlog.read()
-                new = []
-                for item in content.split('|'):
-                    new.append(item)
-                return new
+    with DbManager(**DBCONGIF) as cursor:
+        LOG_SQL ='''INSERT INTO  log (Action_done,username) VALUES(%s,%s)'''
+        cursor.execute(LOG_SQL,('VIEW LOG',username))
+        
+        if username in users:
+            if password == users[username]:
+                SQL = '''SELECT * FROM log'''
+                cursor.execute(SQL)
+                log_data = cursor.fetchall()
+                return log_data
+            else:
+                response= "WRONG PASSWORD!!"
+                return response
         else:
-            return 'Wrong password'
-    else:
-        abort(401)
+            abort(401)
 #FUNCTION TO UPDATE LOG
 #ADD CODE TO CHECK IF ADMIN OR SUPERUSER
 def update_log(update):
-    log_details('admin' ,'update log')
     with open('log.log','w') as vlog:
         updates = input('New stuff: ')
         print(f'These are the new updates by admin\n{updates} \nOn: {time_stamp}|', file=vlog,end='\n')
@@ -87,35 +90,41 @@ def update_log(update):
 
 #CREATE POST
 def create_post(author,title,content)-> None:
-    log_details(author,'Create Post')
     with DbManager(**DBCONGIF) as cursor:
+        LOG_SQL ='''INSERT INTO  log (Action_done,username) VALUES(%s,%s)'''
+        cursor.execute(LOG_SQL,('CREATE POST',author))
         SQL = '''INSERT INTO post(author, title, content) VALUES (%s,%s,%s)'''
         return cursor.execute(SQL,(author,title,content))
     
 #GET POST FROM DB
 def get_post() ->'Posts':
+    #CHECK USER ID AND RETURN PUBLIC AND ALL USER ID'S POST
     with DbManager(**DBCONGIF) as cursor:
-        fetch = '''select * from post order by date DESC '''
-        cursor.execute(fetch)
+        #LOG ACTION ------FIX
+        #LOG_SQL ='''INSERT INTO  log ("GET POST" ,username) VALUES(%s,%s)'''
+        SQL = '''SELECT * FROM post ORDER BY date DESC '''
+        cursor.execute(SQL)
         post = cursor.fetchall()
-        all_post = []
-        for i in range(len(post)):
-            all_post.append(post[i])
-        return all_post
+        return post
 
 def db_search(keyword):
-    with DbManager(**DBCONGIF) as cursor:   
-            SQL_POST = '''SELECT post.content FROM  post WHERE content LIKE  "%s%%" '''
-            cursor.execute(SQL_POST,keyword)
-            posts = cursor.fetchall()
-            SQL_AUTHORS = '''SELECT post.author FROM post WHERE author  LIKE  "%s%%" '''
-            cursor.execute(SQL_AUTHORS,keyword)
-            authors = cursor.fetchall()
-            result = [authors,posts]
-    return result
+    with DbManager(**DBCONGIF) as cursor:  
+        LOG_SQL ='''INSERT INTO  log (Action_done,username) VALUES(%s,%s)'''
+        cursor.execute(LOG_SQL,('SEARCH',keyword))
+        #SQL_POST = """SELECT content FROM  post WHERE content  LIKE '%s%%' """
+        #cursor.execute(SQL_POST,keyword)
+        #posts = cursor.fetchall()
+        SQL_AUTHORS = """SELECT author FROM post WHERE content LIKE  '' """
+        cursor.execute(SQL_AUTHORS)
+        authors = cursor.fetchall()
+        return authors
+        #pprint.pprint(posts)
+        
 
 def  edit_post(id):
     with DbManager(**DBCONGIF) as cursor:
+        LOG_SQL ='''INSERT INTO  log (Action_done,username) VALUES(%s,%s)'''
+        cursor.execute(LOG_SQL,('EDIT POST',id))
         SQL = '''SELECT * FROM post WHERE post_id  = %s'''
         cursor.execute(SQL,(id,))
         post = cursor.fetchall()
@@ -126,6 +135,8 @@ def  edit_post(id):
 
 def delete_post(id):
     with DbManager(**DBCONGIF) as cursor:
+        LOG_SQL ='''INSERT INTO  log (Action_done,username) VALUES(%s,%s)'''
+        cursor.execute(LOG_SQL,('DELETE POST',id))
         delete = '''DELETE FROM post WHERE post_id  = %s'''
         cursor.execute(delete,(id,))
         return redirect(url_for('blog'))        
@@ -158,18 +169,22 @@ def password_gen():
 ##search phrase function
 def search4letters(letter,phrase):
     result = list(set(letter).intersection(set(phrase)))
-    result.sort()
+
     return str(result)[1:-1]
 #calc mi function
 def bmi_calc(name: str,weight:int, height:float):
     bmi = int(weight) // float((height)** 2)
-    with open('log.log', 'a') as user_log:
-        print(f'{name.upper()}, calculated his bmi on {time_stamp} |', file=user_log, end='\n')
-    with open('signup.log', 'a') as bmi_log:
-        print(f'{name.upper()}, of weight {weight} and height {height} \n Got {bmi} as his bmi \n {time_stamp} |',file=bmi_log, end='\n ' )
-        if bmi > 24:
-            return f'{name.capitalize()}:You kinda fat hommie --- {bmi}'
-        elif bmi < 12:
-            return f'{name.capitalize()}: Eey you underweight bro --- {bmi}'
-        else:
-            return f'{name.capitalize()}: Bro you winning at this bmi shit --- {bmi}'
+    if bmi > 24:
+        return f'{name.capitalize()}:You kinda fat hommie --- {bmi}'
+    elif bmi < 12:
+        return f'{name.capitalize()}: Eey you underweight bro --- {bmi}'
+    else:
+        return f'{name.capitalize()}: Bro you winning at this bmi shit --- {bmi}'
+
+
+
+'''post = get_post()
+date = post[0][4] 
+
+print(datetime.strftime(date,'%c'))
+USER FRIENDLY TIME FORMAT WITH TIME SAVED IN DB'''
